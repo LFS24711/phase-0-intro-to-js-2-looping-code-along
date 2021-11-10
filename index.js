0ad4ce71aabfd98e9812122ce7cf4d5a3efec4de
Logan <loganfranklinsmith@hotmail.com>
2021-11-10 16:40:43 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function writeCards(names, ocassion) {
+    let thank =[];
+
+    for (let i = 0; i < names.length; i++) {
+        thank.push(`Thank you, ${names[i]}, for the wonderful ${ocassion} gift!`)
+    };
+
+    return thank;
+};
+
+function countDown(count) {
+    let number = count;
+    
+    while (number >=0){
+        console.log(number--);
+    }
+}
